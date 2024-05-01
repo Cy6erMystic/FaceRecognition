140af68926d9c1eb255436afedb825be32238366
mupsy <amm@mupsy.net>
2024-04-30 20:12:16 -0400

    20240501: work archive

--- datasets/dataset_cfd.py
+++ datasets/dataset_cfd.py
@@ -18,12 +18,26 @@ class CFDDataset(Dataset):
         self._col = col
         self._col_mean = self._df[col].mean()
         self._col_std = self._df[col].std()
+        
+        self._preload()
 
     def __getitem__(self, index):
         row = self._df.iloc[self._idx[index], :]
-        img = cv2.imread(os.path.join(self._root_dir, "face", row["filename"]), cv2.IMREAD_COLOR)
+        # img = cv2.imread(os.path.join(self._root_dir, "face", row["filename"]), cv2.IMREAD_COLOR)
+        img = self._imgs[index]
         # rgb, w, h
         return np.float32(img).transpose([2, 0, 1]), 1 if row[self._col] > self._col_mean else 0
 
     def __len__(self):
         return len(self._idx)
+
+    def _preload(self):
+        imgs = []
+        for i, row in self._df.iloc[self._idx, :].iterrows():
+            img_path = os.path.join(self._root_dir, "face", row["filename"])
+            img = self._get_img(img_path)
+            imgs.append(img)
+        self._imgs = imgs
+
+    def _get_img(self, path: str):
+        return cv2.imread(path, cv2.IMREAD_COLOR)
