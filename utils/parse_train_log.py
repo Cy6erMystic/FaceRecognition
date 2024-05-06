d1dabf58526580be8fb24178e4fe0c245de35833
mupsycho <amm@mupsy.net>
2024-05-06 16:54:24 +0800

    20240506: work archive

--- utils/parse_train_log.py
+++ utils/parse_train_log.py
@@ -12,5 +12,6 @@ def get_data_from_file(path: str):
         for fc in file_contents:
             if fc.find("BEST_ACC") >= 0:
                 best_acc = float(re.findall(r"BEST_ACC:\s(.*?)\n", fc)[0])
+                break
     _, col_name, model_name, param1, param2, param3 = os.path.basename(path).replace(".logs", "").split("_")
     return col_name, model_name, int(param1), float(param2), float(param3), best_acc
