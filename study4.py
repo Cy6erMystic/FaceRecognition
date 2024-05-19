58fc5daf0fb971782761cd1257e4435fea54c56a
mupsy <amm@mupsy.net>
2024-05-19 01:39:17 -0400

    20240519: study3 finished

--- study4.py
+++ study4.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 from datasets.dataset_cfd import CFDDataset
 from utils.rcic import RCIC
@@ -25,33 +26,106 @@ def step1():
             local_embeddings: torch.Tensor = backbone(img.cuda())
             embeddings.append(local_embeddings.cpu())
             labels.append(local_labels.cpu())
-    embeddings: torch.Tensor = torch.concat(embeddings, axis = 0)
-    labels: torch.Tensor = torch.concat(labels, axis = 0)
+    embeddings: torch.Tensor = torch.concat(embeddings, dim = 0)
+    labels: torch.Tensor = torch.concat(labels, dim = 0)
 
     feature_class = torch.concat([embeddings[labels == 0].mean(dim = 0).unsqueeze(0),
-                                  embeddings[labels == 1].mean(dim = 0).unsqueeze(0)], axis = 0)
+                                  embeddings[labels == 1].mean(dim = 0).unsqueeze(0)], dim = 0)
+    torch.save(feature_class, "work_dirs/test/R017/lmcl/1/0.0/0.5/feature2.pt")
 
     coss = []
     for i in range(embeddings.shape[0]):
-        cos = torch.cosine_similarity(embeddings[i], feature_class, axis = 1).unsqueeze(0)
+        cos = torch.cosine_similarity(embeddings[i], feature_class, dim = 1).unsqueeze(0)
         coss.append(cos)
     coss = torch.concat(coss, dim = 0)
     # 得到模型识别效果不好的id索引 以及文件名称
     return coss.max(dim = 1).values.argmin(), \
-           train_set._df["filename"][int(coss.max(dim = 1).values.argmin())]
+           train_set._df["filename"][train_set._idx[int(coss.max(dim = 1).values.argmin())]]
 
+@torch.no_grad()
 def step2():
+    # 获取符合要求的随机噪音
+    torch.cuda.set_device(2)
+    backbone: torch.nn.Module = torch.load("work_dirs/test/R017/lmcl/1/0.0/0.5/bestAcc_model_backbone.pt")
+    backbone.cuda()
+    backbone.eval()
+    features = torch.load("work_dirs/test/R017/lmcl/1/0.0/0.5/feature2.pt")
+
+    get_embed = lambda img_: backbone(torch.tensor(img_.transpose([2,0,1]), dtype=torch.float32).unsqueeze(0).cuda()).cpu()
+    get_cosine = lambda embed_: torch.cosine_similarity(embed_, features, dim = 1)
+
     filename = "0_CFD-LF-236-221-N.jpg"
     im = cv2.imread("../../datasets/1/face/{}".format(filename), cv2.IMREAD_COLOR) # bgr
     im: np.ndarray = ((im / 255) - 0.5) / 0.5
 
-    nosie_patchs, nosie_wights = RCIC.general_noise(112)
-    noise = (nosie_patchs * nosie_wights).mean(axis=2) / 0.3
+    raw_cos_sim = get_cosine(get_embed(im))
+    more = [] # 更像
+    less = []
+    for _ in tqdm(range(10000)):
+        noise = RCIC.general_noise_stand(112)
 
-    img = np.zeros(im.shape)
-    img[:,:,0] = np.mean([im[:,:,0], noise], axis = 0)
-    img[:,:,1] = np.mean([im[:,:,1], noise], axis = 0)
-    img[:,:,2] = np.mean([im[:,:,2], noise], axis = 0)
+        img_add = np.zeros(im.shape)
+        img_reduce = np.zeros(im.shape)
+        img_add[:,:,0] = np.mean([im[:,:,0], noise], axis = 0)
+        img_add[:,:,1] = np.mean([im[:,:,1], noise], axis = 0)
+        img_add[:,:,2] = np.mean([im[:,:,2], noise], axis = 0)
+        img_reduce[:,:,0] = np.mean([im[:,:,0], -noise], axis = 0)
+        img_reduce[:,:,1] = np.mean([im[:,:,1], -noise], axis = 0)
+        img_reduce[:,:,2] = np.mean([im[:,:,2], -noise], axis = 0)
+
+        noise_cos_sim_add = get_cosine(get_embed(img_add))
+        noise_cos_sim_reduce = get_cosine(get_embed(img_reduce))
+        if noise_cos_sim_add[1] > noise_cos_sim_reduce[1]:
+            more.append(noise)
+        else:
+            less.append(noise)
+    np.save("./work_dirs/study4/more.npy", np.array(more))
+    np.save("./work_dirs/study4/less.npy", np.array(less))
+
+def step3():
+    filename = "0_CFD-LF-236-221-N.jpg"
+    im = cv2.imread("../../datasets/1/face/{}".format(filename), cv2.IMREAD_COLOR) # bgr
+    im: np.ndarray = ((im / 255) - 0.5) / 0.5
+
+    noise = RCIC.normalized_noise(np.load("./work_dirs/study4/less.npy").mean(axis = 0))
+    img_add = np.zeros(im.shape)
+    img_reduce = np.zeros(im.shape)
+    img_add[:,:,0] = np.mean([im[:,:,0], noise], axis = 0)
+    img_add[:,:,1] = np.mean([im[:,:,1], noise], axis = 0)
+    img_add[:,:,2] = np.mean([im[:,:,2], noise], axis = 0)
+    img_reduce[:,:,0] = np.mean([im[:,:,0], -noise], axis = 0)
+    img_reduce[:,:,1] = np.mean([im[:,:,1], -noise], axis = 0)
+    img_reduce[:,:,2] = np.mean([im[:,:,2], -noise], axis = 0)
+
+    plt.figure()
+    plt.imshow(img_add[:,:,[2,1,0]] / 2 + 0.5)
+    plt.axis("off")
+    plt.savefig("./work_dirs/study4/add.jpg", bbox_inches = "tight")
+
+    plt.figure()
+    plt.imshow(img_reduce[:,:,[2,1,0]] / 2 + 0.5)
+    plt.axis("off")
+    plt.savefig("./work_dirs/study4/reduce.jpg", bbox_inches = "tight")
+
+def step4():
+    filename = "0_CFD-LF-236-221-N.jpg"
+    im = cv2.imread("../../datasets/1/face/{}".format(filename), cv2.IMREAD_COLOR) # bgr
+    im: np.ndarray = ((im / 255) - 0.5) / 0.5
+
+    cmap1 = ListedColormap([[0,0,0,1],
+                           [1,0,0,1]])
+    cmap2 = ListedColormap([[0,0,0,1],
+                           [0,1,0,1]])
+
+    z1,c1,n1 = RCIC.calc_noise_cluster(np.load("./work_dirs/study4/more.npy").mean(axis = 0))
+    z2,c2,n2 = RCIC.calc_noise_cluster(np.load("./work_dirs/study4/less.npy").mean(axis = 0))
+
+    plt.figure()
+    plt.axis("off")
+    plt.imshow((im[:,:,[2,1,0]] + 1) / 2)
+    plt.imshow(c1, cmap = cmap1, alpha=0.2)
+    plt.imshow(c2, cmap = cmap2, alpha=0.2)
+    plt.savefig("./work_dirs/study4/cluster.jpg", bbox_inches = "tight")
 
 if __name__ == "__main__":
-    step1()
+    step4()
--- utils/rcic.py
+++ utils/rcic.py
@@ -1,21 +1,20 @@
 from __future__ import annotations
 import numpy as np
+from scipy.ndimage import gaussian_filter, label
 
 class RCIC():
     _SCALES = [2 ** i for i in range(5)]
     _ORIENTATIONS = [np.pi * r / 180 for r in [0, 30, 60, 90, 120, 150]]
     _PHASES = [0, np.pi / 2]
-    def __init__(self) -> None:
-        pass
 
     @classmethod
-    def general_noise(cls, img_size: int):
+    def general_noise(cls, img_size: int) -> tuple[np.ndarray]:
         patches = np.zeros((img_size, img_size, len(cls._SCALES) * len(cls._ORIENTATIONS) * len(cls._PHASES)), dtype="float32")
         weights = np.zeros((img_size, img_size, len(cls._SCALES) * len(cls._ORIENTATIONS) * len(cls._PHASES)), dtype="float32")
 
         col = 0
         idx = 0
-        d = np.random.uniform(-1, 1, size = np.sum(len(cls._ORIENTATIONS) * len(cls._PHASES) * np.array([2 ** i for i in range(len(cls._SCALES))]) ** 2))
+        uniform_weights = np.random.uniform(-1, 1, size = np.sum(len(cls._ORIENTATIONS) * len(cls._PHASES) * np.array([2 ** i for i in range(len(cls._SCALES))]) ** 2))
         for scale in cls._SCALES:
             for orientation in cls._ORIENTATIONS:
                 for phase in cls._PHASES:
@@ -30,7 +29,36 @@ class RCIC():
                         for r in range(scale):
                             weights[(size * r):(size * (r + 1) + 1),
                                     (size * c):(size * (c + 1) + 1),
-                                    col] = d[idx]
+                                    col] = uniform_weights[idx]
                             idx += 1
                     col += 1
         return patches, weights
+
+    @classmethod
+    def normalized_noise(cls, noise: np.ndarray) -> np.ndarray:
+        noise = noise.copy()
+        m = (np.max(noise) + np.min(noise)) / 2
+        noise = noise - m
+        return noise / np.max(np.abs(noise))
+
+    @classmethod
+    def general_noise_stand(cls, img_size: int) -> np.ndarray:
+        # 将值归一化，范围为 [-1, 1]
+        noise = np.multiply(*cls.general_noise(img_size)).mean(axis = 2)
+        return cls.normalized_noise(noise)
+    
+    @classmethod
+    def calc_noise_cluster(cls, img: np.ndarray):
+        # 使用之前的方法，进行像素丛聚分析
+        p_value = 0.05
+        sigma = 4
+        # 高斯平滑
+        smoothed_image = gaussian_filter(img, sigma = sigma)
+        # 转换为Z分数
+        mean = np.mean(smoothed_image)
+        std = np.std(smoothed_image)
+        zscore = (smoothed_image - mean) / std
+
+        threshold = np.percentile(zscore, 100 * (1 - p_value))
+        clusters, num_clusters = label(zscore > threshold)
+        return zscore, clusters, num_clusters
